ee3aea014e28c511a4ce51e53d421d69063e1c5b
sc-likes-to-code <137634522+sc-likes-to-code@users.noreply.github.com>
2023-11-25 00:36:48 +0530

    Update arithmetic calculator.py

--- arithmetic calculator.py
+++ arithmetic calculator.py
@@ -1,73 +1,78 @@
-import math
-import random
-print("*****ARITHMETIC CALCULATOR*****")
-print("Enter your choice: \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Modulo\n6. Exponentiation")
-ch=int(input("Enter your choice(1-6):\n"))
-a,b=input("Enter two numbers to be operated on: ").split()
-s=0.0
-d=True
-w='n'
-while d:
-    if ch==1:
-        s=float(a)+float(b)
-        print("Sum of the two numbers is: ",s)
-        w=input("Want to do something more?(y/n)")
-        if w.lower()=='y':
-            ch=int(input("Enter your choice(1-6):\n"))
-            a,b=input("Enter two numbers to be operated on: ").split()
-        else:
-            d=False
-            print("*****Thanks for using*****")
-    elif ch==2:
-        s=float(a)-float(b)
-        print("Difference of the two numbers is: ",s)
-        w=input("Want to do something more?(y/n)")
-        if w.lower()=='y':
-            ch=int(input("Enter your choice(1-6):\n"))
-            a,b=input("Enter two numbers to be operated on: ").split()
-        else:
-            d=False
-            print("*****Thanks for using*****")
-    elif ch==3:
-        s=float(a)*float(b)
-        print("Product of the two numbers is: ",s)
-        w=input("Want to do something more?(y/n)")
-        if w.lower()=='y':
-            ch=int(input("Enter your choice(1-6):\n"))
-            a,b=input("Enter two numbers to be operated on: ").split()
-        else:
-            d=False
-            print("*****Thanks for using*****")
-    elif ch==4:
-        s=float(a)/float(b)
-        print("Quotient is: ",s)
-        w=input("Want to do something more?(y/n)")
-        if w.lower()=='y':
-            ch=int(input("Enter your choice(1-6):\n"))
-            a,b=input("Enter two numbers to be operated on: ").split()
-        else:
-            d=False
-            print("*****Thanks for using*****")
-    elif ch==5:
-        c=int(a)%int(b)
-        print(a,"modulo",b,"= ",c)
-        w=input("Want to do something more?(y/n)")
-        if w.lower()=='y':
-            ch=int(input("Enter your choice(1-6):\n"))
-            a,b=input("Enter two numbers to be operated on: ").split()
-        else:
-            d=False
-            print("*****Thanks for using*****")
-    elif ch==6:
-        c=int(a)**int(b)
-        print(a, "**",b,"=",c)
-        w=input("Want to do something more?(y/n)")
-        if w.lower()=='y':
-            ch=int(input("Enter your choice(1-6):\n"))
-            a,b=input("Enter two numbers to be operated on: ").split()
-        else:
-            d=False
-            print("*****Thanks for using*****")
-    else:
-        print("Wrong choice!!!\nPlease choose something within 1-6")
-        ch=int(input("Enter your choice(1-6):\n"))
+import tkinter as tk
+
+calculation = ""
+
+def add_to_calculation(symbol):
+    global calculation
+    calculation += str(symbol)
+    text_result.delete(1.0, "end")
+    text_result.insert(1.0, calculation)
+    
+def evaluate_calculation():
+    global calculation
+    try:
+        calculation= str(eval(calculation))
+        text_result.delete(1.0, "end")
+        text_result.insert(1.0, calculation)
+    except:
+        clear_field()
+        text_result.insert(1.0, "Error")    
+
+def clear_field():
+    global calculation
+    calculation=""
+    text_result.delete(1.0,"end")
+
+def del_field():
+    global calculation
+    calculation=""
+    text_result.delete(-1.0)
+
+root = tk.Tk()
+root.title("Arithmetic Calculator")
+root.geometry("300x275")
+
+text_result = tk.Text(root, height=2, width=16, font=("Arial",24))
+text_result.grid(columnspan=5)
+
+btn_1= tk.Button(root, text="1", command=lambda: add_to_calculation(1), width=5, font= ("Arial", 14))
+btn_1.grid(row=2, column=1)
+btn_2= tk.Button(root, text="2", command=lambda: add_to_calculation(2), width=5, font= ("Arial", 14))
+btn_2.grid(row=2, column=2)
+btn_3= tk.Button(root, text="3", command=lambda: add_to_calculation(3), width=5, font= ("Arial", 14))
+btn_3.grid(row=2, column=3)
+btn_4= tk.Button(root, text="4", command=lambda: add_to_calculation(4), width=5, font= ("Arial", 14))
+btn_4.grid(row=3, column=1)
+btn_5= tk.Button(root, text="5", command=lambda: add_to_calculation(5), width=5, font= ("Arial", 14))
+btn_5.grid(row=3, column=2)
+btn_6= tk.Button(root, text="6", command=lambda: add_to_calculation(6), width=5, font= ("Arial", 14))
+btn_6.grid(row=3, column=3)
+btn_7= tk.Button(root, text="7", command=lambda: add_to_calculation(7), width=5, font= ("Arial", 14))
+btn_7.grid(row=4, column=1)
+btn_8= tk.Button(root, text="8", command=lambda: add_to_calculation(8), width=5, font= ("Arial", 14))
+btn_8.grid(row=4, column=2)
+btn_9= tk.Button(root, text="9", command=lambda: add_to_calculation(9), width=5, font= ("Arial", 14))
+btn_9.grid(row=4, column=3)
+btn_11= tk.Button(root, text=".", command=lambda: add_to_calculation('.'), width=5, font= ("Arial", 14))
+btn_11.grid(row=5, column=1)
+btn_10= tk.Button(root, text="0", command=lambda: add_to_calculation(0), width=5, font= ("Arial", 14))
+btn_10.grid(row=5, column=2)
+btn_equal= tk.Button(root, text="=", command=evaluate_calculation, width=5, font= ("Arial", 14))
+btn_equal.grid(row=5, column=3)
+btn_plus= tk.Button(root, text="+", command=lambda: add_to_calculation('+'), width=5, font= ("Arial", 14))
+btn_plus.grid(row=2, column=4)
+btn_minus= tk.Button(root, text="-", command=lambda: add_to_calculation('-'), width=5, font= ("Arial", 14))
+btn_minus.grid(row=3, column=4)
+btn_mult= tk.Button(root, text="*", command=lambda: add_to_calculation('*'), width=5, font= ("Arial", 14))
+btn_mult.grid(row=4, column=4)
+btn_div= tk.Button(root, text="/", command=lambda: add_to_calculation('/'), width=5, font= ("Arial", 14))
+btn_div.grid(row=5, column=4)
+btn_open= tk.Button(root, text="(", command=lambda: add_to_calculation('('), width=5, font= ("Arial", 14))
+btn_open.grid(row=6, column=1)
+btn_close= tk.Button(root, text=")", command=lambda: add_to_calculation(')'), width=5, font= ("Arial", 14))
+btn_close.grid(row=6, column=2)
+btn_del= tk.Button(root, text="DEL", command=del_field, width=5, font= ("Arial", 14))
+btn_del.grid(row=6, column=3)
+btn_clear= tk.Button(root, text="C", command=clear_field, width=5, font= ("Arial", 14))
+btn_clear.grid(row=6, column=4)
+root.mainloop()
